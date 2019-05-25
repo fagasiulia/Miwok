afdebf27d2be8351afb7aba7f623f5a6d61d8210
fagas <fagas.iulia@yahoo.com>
2019-05-25 16:53:21 +0300

    Saving the English words for numbers in an array and using this array to set up TextView in the activity_numbers.xml

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.miwok;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +14,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Find the View that shows the family category
+        TextView family = (TextView) findViewById(R.id.family);
+
+        // Set a click listener on that View
+        family.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        // Find the View that shows the colors category
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        // Set a click listener on that View
+        colors.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        // Find the View that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        // Set a click listener on that View
+        phrases.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+
     }
 
-    public void openNumbersList(View v){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
 }
--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -2,6 +2,8 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an array of Strings to store the English words for numbers
+        String[] numbersArray = {"one" , "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        // The following code will use the array above to print the words on the screen
+        LinearLayout roodtView = (LinearLayout)findViewById(R.id.rootView);
+
+        for(int i = 0; i < 10; i++){
+            TextView textView = new TextView(this);
+            textView.setText(numbersArray[i]);
+            roodtView.addView(textView);
+        }
+
     }
 }
