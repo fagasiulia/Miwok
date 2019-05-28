a6b02bd75511682b561a2650f72eba4f01eab036
fagas <fagas.iulia@yahoo.com>
2019-05-28 22:24:22 +0300

    Create a custom ArrayAddapter and populate the Numbers page with Miwok words and default translations.

--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -2,8 +2,13 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,16 +18,38 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // Create an array of Strings to store the English words for numbers
-        String[] numbersArray = {"one" , "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        ArrayList<Word> word = new ArrayList<>();
+        word.add(new Word("one", "lutti"));
+        word.add(new Word("two", "otiiko"));
+        word.add(new Word("three", "tolookosu"));
+        word.add(new Word("four", "oyyisa"));
+        word.add(new Word("five", "massokka"));
+        word.add(new Word("six", "temmokka"));
+        word.add(new Word("seven", "kenekaku"));
+        word.add(new Word("eight", "kawinta"));
+        word.add(new Word("nine", "wo'e"));
+        word.add(new Word("ten", "na'aacha"));
 
-        // The following code will use the array above to print the words on the screen
-        LinearLayout roodtView = (LinearLayout)findViewById(R.id.rootView);
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter itemsAdapter =
+                new WordAdapter(this, word);
 
-        for(int i = 0; i < 10; i++){
-            TextView textView = new TextView(this);
-            textView.setText(numbersArray[i]);
-            roodtView.addView(textView);
-        }
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+
+        ListView listView = findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+
+        listView.setAdapter(itemsAdapter);
 
     }
 }
