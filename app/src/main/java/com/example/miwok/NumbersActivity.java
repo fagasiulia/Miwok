4f0d7e4f9af47d98da688ee8ed7d34f7c65d71ce
fagas <fagas.iulia@yahoo.com>
2019-05-29 19:05:39 +0300

    Add Miwok words and default translations for every page of the app

--- app/src/main/java/com/example/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/miwok/NumbersActivity.java
@@ -2,11 +2,7 @@ package com.example.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,7 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.activity_model);
 
         // Create an array of Strings to store the English words for numbers
         ArrayList<Word> word = new ArrayList<>();
@@ -40,7 +36,7 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // activity_modelml layout file.
 
         ListView listView = findViewById(R.id.list);
 
